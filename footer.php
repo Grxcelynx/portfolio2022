5b3c8ef2acfc6be9c2aa9058d29abb2f72fe0473
Grace Birnam <gbirnam@alphaworks.tech>
2022-06-09 13:58:41 -0700

    basic nav and footer layout

--- footer.php
+++ footer.php
@@ -13,42 +13,8 @@
 
 	<footer id="colophon" class="site-footer">
 		
-			<div class="container">
-				<div class="footer-menu1">
-					<?php 
-						wp_nav_menu(array(
-							'menu' => 'Nav Menu',
-							'theme_location' => 'footer-side1',
-							'menu_class' => 'footer-side1',
-							'menu_id' => 'footer-id'
-						))
-					?>
-				</div>
-
-				<div class="footer-menu2">
-					<?php 
-						wp_nav_menu(array(
-							'menu' => 'Nav Menu',
-							'theme_location' => 'footer-side2',
-							'menu_class' => 'footer-side2',
-							'menu_id' => 'footer-id'
-						))
-					?>
-				</div>
-			</div>
 	
-
-		
-			<div class="footerBox">
-				<p>&copy; Grxcelyn Development</p>
-			</div>
-			<div class="footerBox">
-				<div class="socialMedia">
-					<a href="https://www.instagram.com/?hl=en" target="_blank"><i class="fa-brands fa-instagram fa-2x"></i></a>
-					<a href="https://twitter.com/i/flow/login" target="_blank"><i class="fa-brands fa-twitter-square fa-2x"></i></a>
-					<a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook-square fa-2x"></i></a>
-				</div>
-			</div>
+  
 		
 	</footer><!-- #colophon -->
 </body>
