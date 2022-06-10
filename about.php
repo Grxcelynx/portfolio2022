ad3f374490d250965229d229738aeafdce2eb36c
Grace Birnam <gbirnam@alphaworks.tech>
2022-06-10 16:01:45 -0700

    main base of about done

--- about.php
+++ about.php
@@ -7,4 +7,30 @@
 */
 ?>
 <?php get_header(); ?>
+    <div>
+        <section class="about-img-sec">
+            <div class="about-img">
+                <img src="http://grxcelyn2022.local/wp-content/uploads/2022/06/IMG_9628.jpg" alt="">
+            </div>
+        </section>
+
+        <section class="about-info">
+            <div class="info-text-about">
+                <h1>
+                    Grace 'Grxcelyn' Birnam
+                </h1>
+                <h3>
+                    Artist & Developer
+                </h3>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus labore reprehenderit, repellendus soluta enim impedit aspernatur tempora delectus quaerat voluptate eius in, ab eaque temporibus doloremque optio tenetur veniam accusantium?
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus labore reprehenderit, repellendus soluta enim impedit aspernatur tempora delectus quaerat voluptate eius in, ab eaque temporibus doloremque optio tenetur veniam accusantium?
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus labore reprehenderit, repellendus soluta enim impedit aspernatur tempora delectus quaerat voluptate eius in, ab eaque temporibus doloremque optio tenetur veniam accusantium?
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus labore reprehenderit, repellendus soluta enim impedit aspernatur tempora delectus quaerat voluptate eius in, ab eaque temporibus doloremque optio tenetur veniam accusantium?
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus labore reprehenderit, repellendus soluta enim impedit aspernatur tempora delectus quaerat voluptate eius in, ab eaque temporibus doloremque optio tenetur veniam accusantium?
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus labore reprehenderit, repellendus soluta enim impedit aspernatur tempora delectus quaerat voluptate eius in, ab eaque temporibus doloremque optio tenetur veniam accusantium?
+                </p>
+            </div>
+        </section>
+    </div>
 <?php get_footer(); ?>
