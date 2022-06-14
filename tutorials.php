5214ffcb1df48d0499221dc2553858e13f5ded03
Grace Birnam <gbirnam@alphaworks.tech>
2022-06-14 16:21:19 -0700

    tutorials base and responsiveness done

--- tutorials.php
+++ tutorials.php
@@ -7,4 +7,72 @@
 */
 ?>
 <?php get_header(); ?>
+
+<h1>Grxcelyn Tutorials</h1>
+
+<div class="tut-grid">
+
+    <div class="github">
+        <h2>GitHub</h2>
+        <hr>
+        <ul>
+            <h4>Learn the basics of GitHub</h4>
+            <li>Creating an account</li>
+            <li>SSH Keys</li>
+            <li>Cloning</li>
+            <li>Branches</li>
+            <li>Errors</li>
+        </ul>
+        <hr>
+        <button>
+            <a href="">Watch now</a>
+        </button>
+    </div>
+
+    <div class="html-t">
+        <h2>HTML</h2>
+        <hr>
+        <ul>
+            <h4>Learn the basics of HTML</h4>
+            <li>Tags</li>
+            <li>Grids</li>
+            <li>Forms</li>
+            <li>Navbar</li>
+            <li>And More!</li>
+        </ul>
+        <hr>
+        <button>
+            <a href="">Coming Soon</a>
+        </button>
+    </div>
+
+    <div class="css-t">
+        <h2>CSS</h2>
+        <hr>
+        <ul>
+            <h4>Learn the basics of CSS</h4>
+            <li>Classes & IDs</li>
+            <li>Colors</li>
+            <li>Fonts</li>
+            <li>Sizing</li>
+            <li>And more!</li>
+        </ul>
+        <hr>
+        <button>
+            <a href="">Coming Soon</a>
+        </button>
+    </div>
+    
+</div>
+
+<div class="about-tut">
+    <h2>Lorem ipsum</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi laudantium exercitationem qui ea repudiandae ab dicta magni facilis recusandae quisquam. Illo libero exercitationem quibusdam asperiores fugiat maiores veniam id magni.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi laudantium exercitationem qui ea repudiandae ab dicta magni facilis recusandae quisquam. Illo libero exercitationem quibusdam asperiores fugiat maiores veniam id magni.
+    </p>
+    <button>
+        Visit Channel
+    </button>
+</div>
 <?php get_footer(); ?>
