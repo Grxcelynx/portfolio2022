f465f1158ed3dd6ad1aa73ce30017f41f65b3e14
Grace Birnam <gbirnam@alphaworks.tech>
2022-06-14 16:58:55 -0700

    artwork base half done

--- art.php
+++ art.php
@@ -7,4 +7,37 @@
 */
 ?>
 <?php get_header(); ?>
+
+<div class="artwork-grid">
+    <div class="gallery-preview">
+        <div>gallery</div>
+        <button></button>
+    </div>
+
+    <div class="shop-prev">
+        etsy page preview link
+        <a href="http://">
+        <img src="" alt="">
+        </a>
+    </div>
+
+    <div class="inquiry-ex">
+        <h2>Interested in commissioning Grxcelyn Arts?</h2>
+        <hr>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo itaque consequuntur corrupti inventore dicta, eum quis nostrum a ducimus quam ipsam sed labore iste eveniet perspiciatis quia quas enim eaque!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo itaque consequuntur corrupti inventore dicta, eum quis nostrum a ducimus quam ipsam sed labore iste eveniet perspiciatis quia quas enim eaque!
+        </p>
+        <button>Book now</button>
+    </div>
+
+    <div class="reviews">
+        <h2>Reviews</h2>
+        <hr>
+        <button>
+            <a href="http://"></a>
+        </button>
+    </div>
+</div>
+
 <?php get_footer(); ?>
