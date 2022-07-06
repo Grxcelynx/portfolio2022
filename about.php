2ded6564dfcd4fd394befa608e529d54f886a7fa
Grace Birnam <gbirnam@alphaworks.tech>
2022-07-05 17:02:30 -0700

    added new about

--- about.php
+++ about.php
@@ -9,16 +9,15 @@
 <div class="" id="topBtn"></div>
 
 <?php get_header(); ?>
-
+<!-- 
 <div class="about-grid">
         <div class="heading">
-            <!-- <h1 class="about-title">About</h1> -->
             <h1 class="about-h1">Grace Birnam</h1>
             <h3>Artist & Developer</h3>
         </div>
 
         <div class="profile">
-            <img src="" alt="">
+            <img src="http://grxcelyn2022.local/wp-content/uploads/2022/06/IMG_9628.jpg" alt="">
         </div>
 
         <div class="who">
@@ -43,8 +42,62 @@
             </p>
         </div>
     
-</div>
+</div> -->
 
+
+<div class="strips">
+    <div class="strip hoverin">
+        +
+        <div class="content">
+            <img class="title-img" src="http://grxcelyn2022.local/wp-content/uploads/2022/07/grxclyn-logo-3.png" alt="">
+            <img src="http://grxcelyn2022.local/wp-content/uploads/2022/06/IMG_9628.jpg" alt="">
+        </div>
+    </div>
+    <div class="strip">
+        <div class="plus">+</div>
+        <div class="content">
+            <h1 class="about-h1">Grace Birnam</h1>
+            <h3>Artist & Developer</h3>
+        </div>
+    </div>
+    <div class="strip">
+        <div class="plus">+</div>
+        <div class="content">
+            <h1>Who I Am</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda obcaecati unde excepturi vero facilis pariatur libero recusandae sed distinctio magni quia iusto accusantium reprehenderit, odio ipsum fuga saepe. Consequuntur, nostrum!
+            </p>
+        </div>
+    </div>
+    <div class="strip">
+        <div class="plus">+</div>
+        <div class="content">
+            <h1>What I Do</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto dolor ratione fuga voluptate! Saepe mollitia, facilis dolorem ullam quos voluptatum repudiandae pariatur ducimus eligendi, recusandae obcaecati itaque voluptates officiis magni!
+            </p>
+        </div>
+    </div>
+    <div class="strip">
+        <div class="plus">+</div>
+        <div class="content">
+            <h1>
+            "Failure Is Only The Opportunity To Try Again, Only More Wisely This Time" 
+            <br>
+            - Iroh
+            </h1>
+        </div>
+    </div>
+    <div class="strip">
+        <div class="plus">+</div>
+        <div class="content">
+            <h1>Lumbersexual</h1>
+            <p>Af portland aesthetic, poutine roof party narwhal iPhone keffiyeh gentrify hammock synth occupy church-key chia. Tbh pinterest affogato, lyft mustache whatever portland synth offal. Actually roof party raw denim yuccie, af fanny pack blue
+                bottle cold-pressed forage try-hard williamsburg 8-bit sustainable craft beer. Listicle ethical pork belly post-ironic man braid kinfolk. Tumeric beard cornhole, microdosing tacos VHS occupy kogi vinyl biodiesel XOXO pitchfork mixtape
+                pinterest live-edge. Kombucha tacos cardigan cliche vinyl cold-pressed. Yr schlitz chartreuse raw denim 8-bit, hashtag VHS snackwave bushwick brooklyn raclette aesthetic.</p>
+        </div>
+    </div>
+</div>
 <?php get_template_part('template-parts/top-btn')?> 
 
 <?php get_footer(); ?>
